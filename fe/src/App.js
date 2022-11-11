877ca81074cfce11875af6d17d259dee27358fe9
ragg09-sunasterisk <115448429+ragg09-sunasterisk@users.noreply.github.com>
2022-11-11 15:51:07 +0800

    [SELS-TASK] FE-create-homepage-that-welcomes-users (#1)
    * FE-create-homepage-that-welcomse-users
    
    * added pull request template (2nd commit)
    
    * commit fixes
    
    * Update pull_request_template.md
    
    * comment's fixes
    
    * added simple routes logic

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import Home from "./pages";
+import UserDashboard from "./pages/user";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const token = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          {token === false ? (
+            <>
+              <Route path="/" element={<Home />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<UserDashboard />} />
+            </>
+          )}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ fe/src/pages/user/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div> user's dashboard</div>;
+}
+
+export default index;
