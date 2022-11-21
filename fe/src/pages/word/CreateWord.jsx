d4d674e6d4a9eac3c297910c6c4be9b316be3c22
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2022-11-21 17:23:06 +0800

    [SELS-TASK] FE-Store-Word-Modal

--- fe/src/pages/word/CreateWord.jsx
+++ fe/src/pages/word/CreateWord.jsx
@@ -1,13 +1,82 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addAction, getOneAction } from "../../redux/actions/actions";
+import * as actionType from "../../redux/actions/actionTypes";
 
 function CreateWord(props) {
+  const [data, setData] = useState({
+    lesson_id: "",
+    title: "",
+    hint: "",
+  });
+
+  const [dataOptions, setDataOptions] = useState({
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+  });
+
+  const { category } = useSelector((state) => state.categories);
+
+  const dispatch = useDispatch();
+
   const handleGetCategory = (e) => {
-    console.log(props.id);
+    dispatch(getOneAction(`api/lessons/${props.id}`, actionType.GET_CATEGORY));
+  };
+
+  useEffect(() => {
+    if (category !== {}) {
+      setData({
+        lesson_id: category.id,
+      });
+    }
+  }, [category]);
+
+  const handleInput = (e) => {
+    e.persist();
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleInputOptions = (e) => {
+    e.persist();
+    setDataOptions({ ...dataOptions, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const options = [];
+
+    Object.values(dataOptions).map((value, index) => options.push(value));
+
+    const postData = {
+      lessons_id: data.lesson_id,
+      title: data.title,
+      hint: data.hint,
+      option: options,
+    };
+
+    dispatch(
+      addAction(postData, "api/words", actionType.ADD_WORDS, "api/lessons")
+    );
+
+    setData({
+      lesson_id: "",
+      title: "",
+      hint: "",
+    });
+
+    setDataOptions({
+      option1: "",
+      option2: "",
+      option3: "",
+      option4: "",
+    });
   };
 
   return (
     <Fragment>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="">
           <button
             type="button"
@@ -30,7 +99,7 @@ function CreateWord(props) {
               <div className="modal-content">
                 <div className="modal-header">
                   <h5 className="modal-title" id="createWordModalLabel">
-                    Add Word To CATEGORY_NAME
+                    Add Word To {category.title}
                   </h5>
                   <button
                     type="button"
@@ -40,37 +109,147 @@ function CreateWord(props) {
                   ></button>
                 </div>
                 <div className="modal-body">
-                  <div className="mb-3">
-                    <label htmlFor="title" className="form-label">
-                      Title
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="title"
-                      aria-describedby="emailHelp"
-                      name="title"
-                      data-name="title"
-                    />
-                    <div id="titleHelp" className="form-text">
-                      Please enter lesson name
-                    </div>
-                  </div>
+                  <div className="">
+                    <div className="row">
+                      <div className="col-sm-12 col-md-12 col-lg-5  p1">
+                        <div className="p-2 m-2">
+                          <div className="mb-3">
+                            <h5>Word Details</h5>
+                          </div>
 
-                  <div className="mb-3">
-                    <label htmlFor="description" className="form-label">
-                      Description
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="description"
-                      aria-describedby="descriptionHelp"
-                      name="description"
-                      data-name="description"
-                    />
-                    <div id="descriptionHelp" className="form-text">
-                      Description will help user to choose the right lesson
+                          <div className="mb-3" hidden>
+                            <label htmlFor="lesson_id" className="form-label">
+                              lesson_id
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="lesson_id"
+                              aria-describedby="emailHelp"
+                              name="lesson_id"
+                              data-name="lesson_id"
+                              onChange={handleInput}
+                              value={data.lesson_id || ""}
+                            />
+                            <div id="lesson_idHelp" className="form-text">
+                              Please enter lesson name
+                            </div>
+                          </div>
+
+                          <div className="mb-3">
+                            <label htmlFor="title" className="form-label">
+                              Title
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="title"
+                              aria-describedby="emailHelp"
+                              name="title"
+                              data-name="title"
+                              onChange={handleInput}
+                              value={data.title || ""}
+                            />
+                            <div id="titleHelp" className="form-text">
+                              Please enter word name
+                            </div>
+                          </div>
+
+                          <div className="mb-3">
+                            <label htmlFor="hint" className="form-label">
+                              Hint
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="hint"
+                              aria-describedby="emailHelp"
+                              name="hint"
+                              data-name="hint"
+                              onChange={handleInput}
+                              value={data.hint || ""}
+                            />
+                            <div id="hintHelp" className="form-text">
+                              Enter hint for additional help.
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-sm-12 col-md-12 col-lg-7  p1">
+                        <div className="p-2 m-2">
+                          <div className="mb-3">
+                            <h6>Options</h6>
+                          </div>
+
+                          <div className="mb-3 border rounded p-2 bg-light">
+                            <label htmlFor="option1" className="form-label">
+                              Option 1
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="option1"
+                              aria-describedby="emailHelp"
+                              name="option1"
+                              data-name="option1"
+                              onChange={handleInputOptions}
+                              value={dataOptions.option1 || ""}
+                            />
+                            <div id="hintHelp" className="form-text">
+                              Please Be Noted That This Option Must Be The Right
+                              Translation
+                            </div>
+                          </div>
+
+                          <div className="mb-3">
+                            <label htmlFor="option2" className="form-label">
+                              Option 2
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="option2"
+                              aria-describedby="emailHelp"
+                              name="option2"
+                              data-name="option2"
+                              onChange={handleInputOptions}
+                              value={dataOptions.option2 || ""}
+                            />
+                          </div>
+
+                          <div className="mb-3">
+                            <label htmlFor="option3" className="form-label">
+                              Option 3
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="option3"
+                              aria-describedby="emailHelp"
+                              name="option3"
+                              data-name="option3"
+                              onChange={handleInputOptions}
+                              value={dataOptions.option3 || ""}
+                            />
+                          </div>
+
+                          <div className="mb-3">
+                            <label htmlFor="option4" className="form-label">
+                              Option 4
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="option4"
+                              aria-describedby="emailHelp"
+                              name="option4"
+                              data-name="option4"
+                              onChange={handleInputOptions}
+                              value={dataOptions.option4 || ""}
+                            />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
--- fe/src/redux/reducers/rootReducer.js
+++ fe/src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import categoriesReducers from "./categoriesReducers";
+import wordsReducers from "./wordsReducers";
 
 const rootReducer = combineReducers({
   categories: categoriesReducers,
+  words: wordsReducers,
 });
 
 export default rootReducer;
