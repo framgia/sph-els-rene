41fc61866a76e9984d461eacfcd67f7052ca2980
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2022-11-23 10:14:56 +0800

    Update migration files for activity logs | move localstorage code to utils

--- be/database/migrations/2022_11_23_020637_change_name_loggale_id_to_loggable_id_in_activty_logs_table.php
+++ be/database/migrations/2022_11_23_020637_change_name_loggale_id_to_loggable_id_in_activty_logs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('activity_logs', function (Blueprint $table) {
-            $table->unsignedBigInteger('loggale_id')->change();
+            $table->renameColumn('loggale_id', 'loggable_id');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('activity_logs', function (Blueprint $table) {
-            $table->unsignedBigInteger('loggale_id')->change();
+            $table->renameColumn('loggale_id', 'loggable_id');
         });
     }
 };
