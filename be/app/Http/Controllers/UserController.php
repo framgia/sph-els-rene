b387cea5bc51a18df337d4603b6e2863fb4228e1
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2022-11-25 10:46:48 +0800

    [SELS-TASK] BE-Get-All-User-API #28

--- be/app/Http/Controllers/UserController.php
+++ be/app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::all()->except(1);
         return response([
             'users' => $users,
         ]);
