64795a813f57ae911ed6939194fd7a60bfdf1f68
ragg09-sunasterisk <115448429+ragg09-sunasterisk@users.noreply.github.com>
2022-11-25 14:16:33 +0800

    [SELS-TASK] BE-Get-All-User-API (#28)
    * [SELS-TASK] BE-Get-All-User-API #28
    
    * Add User Seeder File | Update Get All User API Call

--- be/app/Http/Controllers/UserController.php
+++ be/app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::whereNot("role", "admin")->get();
         return response([
             'users' => $users,
         ]);
