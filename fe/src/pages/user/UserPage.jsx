c9f294aedae83182538a1fb6486416d42987e1d9
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2022-11-28 09:53:54 +0800

    [SELS-TASK] FE-User-List-Search

--- fe/src/pages/user/UserPage.jsx
+++ fe/src/pages/user/UserPage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header";
@@ -8,6 +8,8 @@ import { getAllAction } from "../../redux/actions/actions";
 import * as actionType from "../../redux/actions/actionTypes";
 
 function UserPage() {
+  const [search, setSearch] = useState("");
+
   const { users, loading } = useSelector((state) => state.users);
 
   const dispatch = useDispatch();
@@ -25,6 +27,16 @@ function UserPage() {
       <Header />
 
       <div className="container card">
+        <form className="mb-1 mt-3">
+          <input
+            className="form-control me-5"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+
         <table className="table table-hover text-center">
           <thead>
             <tr>
@@ -35,25 +47,36 @@ function UserPage() {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>
-                  <img
-                    className="rounded-circle border border-3 align-self-center"
-                    style={{ width: 100, height: 100 }}
-                    src={user.avatar ?? "/images/default_image.jpg"}
-                    alt="avatar"
-                  />
-                </td>
-                <td className="align-middle">
-                  {user.first_name + " " + user.last_name}
-                </td>
-                <td className="align-middle"> {user.email}</td>
-                <td className="align-middle">
-                  <Link to={`/user/profile/${user.id}`}>View</Link>
-                </td>
-              </tr>
-            ))}
+            {users
+              .filter((user) => {
+                return search.toString().toLowerCase() === ""
+                  ? user
+                  : user.first_name.toString().toLowerCase().includes(search) ||
+                      user.last_name
+                        .toString()
+                        .toLowerCase()
+                        .includes(search) ||
+                      user.email.toString().toLowerCase().includes(search);
+              })
+              .map((user) => (
+                <tr key={user.id}>
+                  <td>
+                    <img
+                      className="rounded-circle border border-3 align-self-center"
+                      style={{ width: 100, height: 100 }}
+                      src={user.avatar ?? "/images/default_image.jpg"}
+                      alt="avatar"
+                    />
+                  </td>
+                  <td className="align-middle">
+                    {user.first_name + " " + user.last_name}
+                  </td>
+                  <td className="align-middle"> {user.email}</td>
+                  <td className="align-middle">
+                    <Link to={`/user/profile/${user.id}`}>View</Link>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
