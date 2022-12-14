2b319d458245730060d8d4dbad3fef594f72bb76
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2022-12-14 13:30:21 +0800

    [BE][refactor] Reconfigure-API-Routes

--- be/routes/api.php
+++ be/routes/api.php
@@ -21,15 +21,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Authentication
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-// Protected Routed
+// Protected Routes
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('lessons', LessonController::class);
